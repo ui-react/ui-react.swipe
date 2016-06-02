0c6811d5f5a5251d75c1f97947f4e5f1873830cc
lamo2k123 <lamo2k123@gmail.com>
2016-06-03 02:05:42 +0300

    change history

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,4 +4,4 @@ import { Router, browserHistory, hashHistory } from 'react-router';
 
 import routes from './routes';
 
-render(<Router routes={routes} history={hashHistory} />, document.getElementById('app'));
+render(<Router routes={routes} history={browserHistory} />, document.getElementById('app'));
